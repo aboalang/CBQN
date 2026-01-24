c39252cac4c47d191c47f418829e7b076c24388f
Marshall Lochbaum <mwlochbaum@gmail.com>
2026-01-24 17:30:34 -0500

    Enable NEON prefix sums for radix sorting

--- src/builtins/radix.h
+++ src/builtins/radix.h
@@ -10,7 +10,7 @@
 #define RDX_SUM_2(T)  GRADE_UD(c1[0]=0;,)             T s0=0, s1=0;             for(usz j=0;j<256;j++) { s0+=c0[j]; s1+=c1[j];                       c0[j]=s0; c1[j]=s1;                     }
 #define RDX_SUM_4(T)  GRADE_UD(c1[0]=c2[0]=c3[0]=0;,) T s0=0, s1=0, s2=0, s3=0; for(usz j=0;j<256;j++) { s0+=c0[j]; s1+=c1[j]; s2+=c2[j]; s3+=c3[j]; c0[j]=s0; c1[j]=s1; c2[j]=s2; c3[j]=s3; }
 
-#if SINGELI_X86_64
+#if SINGELI_SIMD
 extern void (*const si_scan_pluswrap_u8)(uint8_t* v0,uint8_t* v1,uint64_t v2,uint8_t v3);
 extern void (*const si_scan_pluswrap_u32)(uint32_t* v0,uint32_t* v1,uint64_t v2,uint32_t v3);
 #define RADIX_SUM_1_u8   si_scan_pluswrap_u8 (c0,c0,  256,0);
@@ -28,7 +28,7 @@ extern void (*const si_scan_pluswrap_u32)(uint32_t* v0,uint32_t* v1,uint64_t v2,
 #define RADIX_SUM_4_u32  RDX_SUM_4(u32)
 #endif
 
-#if SINGELI_X86_64 && !USZ_64
+#if SINGELI_SIMD && !USZ_64
 #define RADIX_SUM_1_usz  si_scan_pluswrap_u32(c0,c0,  256,0);
 #define RADIX_SUM_2_usz  si_scan_pluswrap_u32(c0,c0,2*256,0);
 #define RADIX_SUM_4_usz  si_scan_pluswrap_u32(c0,c0,4*256,0);
